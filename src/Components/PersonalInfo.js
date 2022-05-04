327155c36efbbb19ca4125103c78771890d97f50
JBe <jmbeaderstadt@gmail.com>
2022-05-04 18:17:24 -0500

    Initial Commit

---
+++ src/Components/PersonalInfo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class PersonalInfo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstName: '',
+            lastName: '',
+            address: '',
+            phoneNumber: '',
+            email: '',
+            website: '',
+
+        }
+    }
+
+    render() {
+        <form>
+
+        </form>
+    }
+}
+
+export default PersonalInfo;
